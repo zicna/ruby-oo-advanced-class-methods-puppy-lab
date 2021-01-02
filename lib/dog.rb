d9a0f74ccf2f245c84e566c4ccaf17b36293cde7
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-01-01 22:35:37 -0600

    commit tree 21e8adf345314732124327167e34fbf6d7d2d4b3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each{|dog| puts dog.name}
+    end
+
+    def save
+        @@all << self
+    end
+
+    def name
+        @name
+    end
+
+
+end
